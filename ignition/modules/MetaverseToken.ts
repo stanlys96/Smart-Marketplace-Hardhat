31a10144e48202ab2325b3ffc4904a7f563b9bd5
stanlys96 <stanlyskwok@gmail.com>
2025-06-23 00:55:58 +0700

    update

--- ignition/modules/MetaverseToken.ts
+++ ignition/modules/MetaverseToken.ts
@@ -1,6 +1,7 @@
 import { ethers } from "ethers";
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
+// Token
 const TokenModule = buildModule("MetaverseToken", (m: any) => {
   const token = m.contract("Metaverse", [
     "Metaverse",
